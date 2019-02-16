7b49ab27815ca321974460a372a7eee025b9fccb
현지 <@DESKTOP-8HQT0RF>
2019-02-17 00:36:28 +0900

    수정

---
+++ KGCinema/src/main/java/com/kg/cinema/MainController.java
@@ -0,0 +1,31 @@
+package com.kg.cinema;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	//@Autowired
+	//MovieDAO mdao = new MovieDAO();
+	
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	public ModelAndView main() {
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+	
+}
